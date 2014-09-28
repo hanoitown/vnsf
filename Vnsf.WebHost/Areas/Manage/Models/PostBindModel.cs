68264bdd60ec2d1cf48e886966fc76e9d45234eb
hanoitown <ha@hanoitown.com>
2014-09-29 00:26:19 +0700

    Save

---
+++ Vnsf.WebHost/Areas/Manage/Models/PostBindModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vnsf.Data.Entities;
+using Vnsf.WebHost.Infrastructure.Mapping;
+
+namespace Vnsf.WebHost.Areas.Manage.Models
+{
+    public class PostBindModel : IMapFrom<Post>
+    {
+        [HiddenInput]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.MultilineText), AllowHtml]
+        public string Abstract { get; set; }
+        [DataType(DataType.MultilineText), AllowHtml]
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Guid CategoryId { set; get; }
+    }
+}
---
+++ Vnsf.WebHost/Areas/Manage/Models/PostViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vnsf.Data.Entities;
+using Vnsf.WebHost.Infrastructure.Mapping;
+
+namespace Vnsf.WebHost.Areas.Manage.Models
+{
+    public class PostViewModel : IMapFrom<Post>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public PostStatus Status { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
